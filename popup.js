c66fa9e801cbdc26cd236833f2e77c6913959142
Silevilence <silevilence@outlook.com>
2019-12-27 20:27:53 +0800

    输入完成后可以使用回车键进行搜索

--- popup.js
+++ popup.js
@@ -1,9 +1,16 @@
 let btn = document.getElementById("search");
+let txt = document.getElementById("txt");
 
 btn.onclick = function () {
-    let txt = document.getElementById("txt");
     // window.open("https://store.steampowered.com/search/?term=baldr%20sky");
     window.open("https://store.steampowered.com/search/?term=" + encodeURI(txt.value)
         .replace("&", "%26").replace("=", "%3D").replace("?", "%3F"));
-    encodeURI(txt.value).replace("&", "%26").replace("=", "%3D").replace("?", "%3F")
+    // encodeURI(txt.value).replace("&", "%26").replace("=", "%3D").replace("?", "%3F")
 }
+
+txt.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        btn.click();
+    }
+});
